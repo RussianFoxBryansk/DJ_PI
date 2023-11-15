4db4cceeb7eab507e57be03d9c63c430fe74188a
RussianFoxBryansk <138440961+RussianFoxBryansk@users.noreply.github.com>
2023-11-15 14:46:54 +0300

    дз ВЫПОЛНИЛ

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'women',
 
 ]
+#'django_extensions',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,4 +1,10 @@
 # для настройки админ-панели сайта
 from django.contrib import admin
+from  .models import Gradebook
+from  .models import Students
+from  .models import Portfolio
 
+admin.site.register(Gradebook)
+admin.site.register(Students)
+admin.site.register(Portfolio)
 # Register your models here.
